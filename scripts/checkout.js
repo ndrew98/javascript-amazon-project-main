6b278c11ae49ad4a064a0d92b76a61d42931235a
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-02-09 14:19:25 +0000

    Delete Products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach(cartItem => {
     }
   });
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -106,7 +106,10 @@ document.querySelectorAll('.js-delete-link')
     const productId = link.dataset.productId;
     // console.log(productId);
     removeFromCart(productId);
-    // const filteredCart = cart.filter(cartItem => {
-    //   return cartItem.productId !== productId;
+
+    //Update html to remove the item from the cart
+    const container = document.querySelector(`.js-cart-item-container-${productId}`)
+    container.remove();
+
     });
   });
