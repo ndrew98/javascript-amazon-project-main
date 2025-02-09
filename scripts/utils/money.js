0fbbfc6d597767c225446cbacdd595cb9df26f59
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-02-09 01:38:32 +0000

    Fix price formatting and radio selectors

---
+++ scripts/utils/money.js
@@ -0,0 +1,10 @@
+//formatting the currency to two decimal places
+
+/**
+ * formats the price to two decimal places
+ * @param {number} priceCents - the price in cents
+ * @returns {string} - the price in dollars
+ */
+export function formatCurrency(priceCents) {
+    return (priceCents / 100).toFixed(2);
+}
