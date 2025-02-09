e8697462e80f96fb6816b0f13e505aba87f01c05
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-02-09 14:46:48 +0000

    Save Cart in LocalStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,27 @@
-export let cart = [
-  {
-    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity : 2,
-  },
-  {
-    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity : 2
-  }
-];
+// export let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+// or 
+
+export let cart = JSON.parse(localStorage.getItem('cart'))
+
+if (!cart) {
+  cart = [
+    {
+      productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity : 2,
+    },
+    {
+      productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity : 1
+    }
+  ];
+};
+
+//save to local storage
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));   
+}
+
 
 //function to add to cart 
 export function addToCart(productId) {
@@ -26,6 +40,8 @@ export function addToCart(productId) {
           quantity : 1
         });
       }
+
+  saveToStorage();
 };
 
 export function removeFromCart(productId) {
@@ -40,4 +56,6 @@ export function removeFromCart(productId) {
 
   cart = newCart
   // console.log(cart)
+
+  saveToStorage();
 }
